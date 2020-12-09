e1fa0f5932a9194dbab29f7bbe4d6170f0d673ce
Matheus Gomes <matheusgomes062@gmail.com>
2020-12-08 21:49:19 -0300

    feat(api): forgot to make the push last time

---
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.specs.ts
@@ -0,0 +1,29 @@
+import { UserData } from '../user-data'
+import { InMemoryUserRepository } from './in-memory-user-repository'
+
+describe('In memory user repository', () => {
+  test('should return null if user is not found', async () => {
+    const users: UserData[] = []
+    const sut = new InMemoryUserRepository(users)
+    const user = await sut.findUserByEmail('any@mail.com')
+    expect(user).toBeNull()
+  })
+
+  test('shuld return user if it is in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@mail.com'
+    const sut = new InMemoryUserRepository(users)
+    await sut.add({ name, email })
+    const user = await sut.findUserByEmail('any@mail.com')
+    expect(user.name).toBe('any_name')
+  })
+
+  test('should return all users in the repository', async () => {
+    const users: UserData[] = [{ name: 'any_name', email: 'any@mail.com' },
+      { name: 'second_name', email: 'second@mail.com' }]
+    const sut = new InMemoryUserRepository(users)
+    const returnedUsers = sut.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
+  })
+})
---
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -0,0 +1,33 @@
+import { UserRepository } from '../ports/user-repository'
+import { UserData } from '../user-data'
+
+export class InMemoryUserRepository implements UserRepository {
+  private repository: UserData[];
+
+  constructor (repository: UserData[]) {
+    this.repository = repository
+  }
+
+  async add (user: UserData): Promise<void> {
+    const exists = await this.exists(user)
+    if (!exists) {
+      this.repository.push(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const found = this.repository.find(user => user.email === email)
+    return found || null
+  }
+
+  async findAllUsers (): Promise<UserData[]> {
+    return this.repository
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    if (await this.findUserByEmail(user.email) == null) {
+      return false
+    }
+    return true
+  }
+}

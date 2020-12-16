973cfe744f2e5e57d831667a29c331f1cca64f69
Matheus Gomes <matheusgomes062@gmail.com>
2020-12-15 21:58:39 -0300

    test: validate domain part larger than 63 chars

--- src/use-cases/entitites/email-validation.spec.ts
+++ src/use-cases/entitites/email-validation.spec.ts
@@ -35,4 +35,9 @@ describe('Email validation', () => {
     const email = '@email.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept domain with a part larger than 63 chars', () => {
+    const email = `any@${'d'.repeat(64)}.com`
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })

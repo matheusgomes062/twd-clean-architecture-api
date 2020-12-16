42ab15854e6054ea1fcba186341fae656a9ff96d
Matheus Gomes <matheusgomes062@gmail.com>
2020-12-15 21:57:48 -0300

    feat: validate empty domain

--- src/use-cases/entitites/email.ts
+++ src/use-cases/entitites/email.ts
@@ -10,11 +10,11 @@ export class Email {
 
     const [local, domain] = email.split('@')
 
-    if (local.length > 64) {
+    if (local.length > 64 || local.length === 0) {
       return false
     }
 
-    if (domain.length > 255) {
+    if (domain.length > 255 || domain.length === 0) {
       return false
     }
     return true

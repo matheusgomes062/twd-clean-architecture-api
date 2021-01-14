31e6c370fb37b19d96f167109fcf56eb51d2a7b6
Matheus Gomes <matheusgomes062@gmail.com>
2021-01-14 19:44:24 -0300

    fix: adicionado configuração do jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**',
+  ],
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    '.+\\.ts$': 'ts-jest',
+  },
+};
